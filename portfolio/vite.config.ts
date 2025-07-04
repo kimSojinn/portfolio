2c869f08007bd1638ece7dd31c67492b52c1f349
Sojin <118108221+kimSojinn@users.noreply.github.com>
2025-07-04 17:11:16 +0900

    Updates

--- portfolio/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
-import svgr from "vite-plugin-svgr";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-  base: "/portfolio/",
-});
